5cffacf88b1bb0a8fd21a78626ae2900444f7a7a
Marcoddx <marco.davidson97@gmail.com>
2017-10-16 22:00:37 +0800

    Delete Rule.cs

--- Launcher_Samp_Public/QueryResult/Rule.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace SampQueryService.QueryResult
-{
-    public struct Rule
-    {
-        public string Name { get; internal set; }
-        public string Value { get; internal set; }
-    }
-}
